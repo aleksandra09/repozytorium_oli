c664f90f14550e88e24775a26d4e44bb499cbdc6
AleksandraJ <aleksandrajankowska09@gmail.com>
2014-05-15 17:10:02 +0200

    dodałam funkcję get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,5 +2,9 @@ def dodawanie():
     a = int(input("Wprowadź pierwszą lizbę "))
     b = int(input("Wprowadź drugą liczbę "))
     print(a+b)
+
+def get_info():
+    print("To jest program, który dodaje liczby")
     
+get_info()
 dodawanie()
